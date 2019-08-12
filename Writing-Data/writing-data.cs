ad6c09a728ae6689ab955c42803af8794756257f
Bill Burn <williamburn@gmail.com>
2019-08-11 20:07:47 -0500

    Console.WriteLine samples

---
+++ Writing-Data/writing-data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Writing_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is some test data 1");
+            Console.WriteLine("This is some test data 2");
+            Console.WriteLine("This is some test data 3");
+        }
+    }
+}
