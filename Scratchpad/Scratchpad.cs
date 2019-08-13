328245623a2e77bc4f5f182769347f078571da12
Bill Burn <williamburn@gmail.com>
2019-08-13 17:08:29 -0500

    adding scratch

---
+++ Scratchpad/Scratchpad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Scratchpad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int students = 18;
+            //int groupSize = students % 3;
+            //Console.Write(groupSize);
+            int myInt = Math.Abs(12932);
+            double mySqrt = Math.Sqrt(myInt);
+            double myFloor = Math.Floor(mySqrt);
+            double myMin = Math.Min(12, 189);
+            Console.WriteLine(myInt);
+            Console.WriteLine(mySqrt);
+            Console.WriteLine(myFloor);
+            Console.WriteLine(myMin);
+        }
+    }
+}
