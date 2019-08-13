57fbc2d0406e92f671b12400b6ad7dc4c36e39c3
Bill Burn <williamburn@gmail.com>
2019-08-13 17:15:06 -0500

    adding powerof

---
+++ ToThePowerOf/Power.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Example
+{
+   public static void Main()
+   {
+      int value = 2;
+      for (int power = 0; power <= 128; power++)
+         Console.WriteLine("{0}^{1} = {2:N0} (0x{2:X})", 
+                           value, power, (long)Math.Pow(value, power));
+   }
+}
