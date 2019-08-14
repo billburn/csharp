074d662b638510bb3994c3925bec51316e8f99d9
Bill Burn <williamburn@gmail.com>
2019-08-14 06:03:13 -0500

    testing string method

--- Scratchpad/Scratchpad.cs
+++ Scratchpad/Scratchpad.cs
@@ -9,14 +9,19 @@ namespace Scratchpad
             //int students = 18;
             //int groupSize = students % 3;
             //Console.Write(groupSize);
-            int myInt = Math.Abs(12932);
-            double mySqrt = Math.Sqrt(myInt);
-            double myFloor = Math.Floor(mySqrt);
-            double myMin = Math.Min(12, 189);
-            Console.WriteLine(myInt);
-            Console.WriteLine(mySqrt);
-            Console.WriteLine(myFloor);
-            Console.WriteLine(myMin);
+            //int myInt = Math.Abs(12932);
+            //double mySqrt = Math.Sqrt(myInt);
+            //double myFloor = Math.Floor(mySqrt);
+            //double myMin = Math.Min(12, 189);
+            //Console.WriteLine(myInt);
+            //Console.WriteLine(mySqrt);
+            //Console.WriteLine(myFloor);
+            //Console.WriteLine(myMin);
+            string withoutSlash = "Ifemelu said, 'Hello!'";
+            string withSlash = "Ifemelu said, \'Hello!\'";
+
+            Console.WriteLine(withoutSlash);
+            Console.Write(withSlash);
         }
     }
 }
