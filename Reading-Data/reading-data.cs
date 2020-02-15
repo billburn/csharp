c3ee51a406ba053de36ddf03c1f27247f9817123
billburn <williamburn@gmail.com>
2020-02-15 05:36:13 -0600

    Minor update

--- Reading-Data/reading-data.cs
+++ Reading-Data/reading-data.cs
@@ -6,7 +6,7 @@ namespace Reading_Data
     {
         static void Main(string[] args)
         {
-            Console.Write("How old are you, enter your age: ");
+            Console.Write("How old are you, please enter your age: ");
             string input = Console.ReadLine();
             Console.WriteLine($"You are {input} years old!");
 
